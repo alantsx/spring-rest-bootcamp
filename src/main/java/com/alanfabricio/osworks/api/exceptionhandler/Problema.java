d8c071f327984837dbe0dff253cb66805fcc9d0c
Alan Pereira <alanpfabricio@gmail.com>
2021-01-15 18:29:08 -0300

    first commit

---
+++ src/main/java/com/alanfabricio/osworks/api/exceptionhandler/Problema.java
@@ -0,0 +1,63 @@
+package com.alanfabricio.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class Problema {
+	private Integer Status;
+	private LocalDateTime dataHora;
+	private String title;
+	private List<Campo> campos;
+	
+	public static class Campo {
+		private String nome;
+		private String mensagem;
+		
+		public Campo(String nome, String mensagem) {
+			super();
+			this.nome = nome;
+			this.mensagem = mensagem;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getMensagem() {
+			return mensagem;
+		}
+		public void setMensagem(String mensagem) {
+			this.mensagem = mensagem;
+		}
+	}
+	
+	public Integer getStatus() {
+		return Status;
+	}
+	public void setStatus(Integer status) {
+		Status = status;
+	}
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public List<Campo> getCampos() {
+		return campos;
+	}
+	public void setCampos(List<Campo> campos) {
+		this.campos = campos;
+	}
+}
